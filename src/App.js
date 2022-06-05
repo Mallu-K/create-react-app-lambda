2759c20a7b9b8071571986e35c8a6454372aebac
Mallu-K <67813522+Mallu-K@users.noreply.github.com>
2022-06-05 17:39:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,90 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Animated,
+  TouchableOpacity
+} from "react-native";
+import { Fade } from "rnal";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+export default class MyComponent extends Component {
+  state = {
+    ready: false,
+    animatedValue: new Animated.Value(0)
+  };
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
+  _start = () => {
+    Animated.timing(this.state.animatedValue, {
+      toValue: 1,
+      duration: 1000
+    }).start();
+  };
   render() {
-    const { loading, msg } = this.state
-
+    let { animatedValue } = this.state;
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.btn} onPress={() => this._start()}>
+          <Text style={styles.textBtn}>Start</Text>
+        </TouchableOpacity>
+        <Animated.View
+          style={{
+            transform: [
+              {
+                translateY: animatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [-600, 0]
+                })
+              }
+            ],
+            height: 250,
+            width: 200,
+            margin: 5,
+            borderRadius: 12,
+            backgroundColor: "#347a2a",
+            justifyContent: "center"
+          }}
+        />
+        <Text style={styles.text}>Fade </Text>
+
+        <Animated.View />
+      </View>
+    );
   }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  item: {},
+  btn: {
+    backgroundColor: "#480032",
+    width: 100,
+    height: 40,
+    padding: 3,
+    justifyContent: "center",
+    borderRadius: 6
+  },
+  text: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  item1: {
+    backgroundColor: "red",
+    padding: 20,
+    width: 100,
+    margin: 10
+  },
 
-export default App
+  textBtn: {
+    color: "#f4f4f4",
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+});
